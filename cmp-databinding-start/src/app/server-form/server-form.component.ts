4244f4ce9222214bffe903471cda2a8d7f0bda6e
MaxwellChrist <ribthulu@gmail.com>
2023-02-08 15:25:28 -0500

    broke out app component into server element and server form components

---
+++ cmp-databinding-start/src/app/server-form/server-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-form',
+  templateUrl: './server-form.component.html',
+  styleUrls: ['./server-form.component.css']
+})
+export class ServerFormComponent implements OnInit {
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer() {
+    // this.serverElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+
+  onAddBlueprint() {
+    // this.serverElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+
+}
