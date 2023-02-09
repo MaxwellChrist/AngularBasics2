9db33f363dc9c78d07f328cf022a5c8bb6a3362c
MaxwellChrist <ribthulu@gmail.com>
2023-02-09 11:37:20 -0500

    removed unnecessary hooks and added ng-content hook to render part of server element component

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,27 +1,14 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
+export class ServerElementComponent implements OnInit  {
   @Input() element: {name: string, type: string, content: string};
 
-  constructor() { 
-    console.log('constructor called')
-  }
+  constructor() { }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges called');
-    console.log(changes)
-  }
-
-  ngOnInit(): void {
-    console.log('ngOnInit called')
-  }
-
-  ngDoCheck(): void {
-    console.log('ngDoCheck called')
-  }
+  ngOnInit(): void {}
 }
